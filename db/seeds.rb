8751c898576dc1f13e00870445a27989a4f68cf8
Vasin Dmitriy <vasindima779@gmail.com>
2015-06-07 09:39:46 +0300

    changed seed and update gitignore

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,10 @@
 Post.create!([ {title: 'title', body: 'this is a blab'}, {title: 'title 2', body: 'this is a blab 2'} ])
 
+User.create!(
+  email: 'user@example.com',
+  username: 'user',
+  password: 'password',
+  password_confirmation: 'password'
+)
+
 puts 'Load seeds complite!'
